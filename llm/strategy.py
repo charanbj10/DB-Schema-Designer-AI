d615e016177c8433858f3a67fcfb8a836fd61db6
Sri charan B J <sricharan.b@vlinder.io>
2026-02-28 19:58:52 +0530

    Strategy Pattern fixed

--- llm/strategy.py
+++ llm/strategy.py
@@ -1,24 +1,7 @@
-import os
-from dotenv import load_dotenv
-from .openai_strategy import OpenAIStrategy
-from .ollama_strategy import OllamaStrategy
-
-load_dotenv()
-
-def get_llm_strategy():
-    provider = os.getenv("LLM_PROVIDER").lower()
-
-    if provider == "openai":
-        return OpenAIStrategy()
-
-    elif provider == "ollama":
-        return OllamaStrategy()
-
-    else:from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 
 class LLMStrategy(ABC):
 
     @abstractmethod
     def chat(self, system_prompt: str, user_prompt: str) -> str:
         pass
-        raise ValueError(f"Unsupported LLM_PROVIDER: {provider}")
